1a45c3a8fb938caab7af9ab297fddbb8e9de6123
NotGyashu <rahmangyashu178@gmail.com>
2025-10-23 11:13:32 +0000

    fix: Refactor Projects component layout by removing unnecessary flex-grow for improved spacing

--- src/components/projects.js
+++ src/components/projects.js
@@ -73,9 +73,9 @@ function Projects() {
         <ScrollTracker ref={scrollRef} />
       </div>
       {/* Main content area - Takes remaining space */}
-      <div className="flex md:gap-5 h-full gap-2 flex-col flex-grow  justify-between p-2">
+      <div className="flex md:gap-5 h-full gap-2 flex-col   justify-between p-2">
       <div
-        className="flex  lg:mx-7 flex-grow  gap-12 lg:gap-28 md:gap-16 justify-between overflow-x-scroll no-scrollbar"
+        className="flex  lg:mx-7   gap-12 lg:gap-28 md:gap-16 justify-between overflow-x-scroll no-scrollbar"
         id="parent"
         ref={scrollRef}
         style={{
